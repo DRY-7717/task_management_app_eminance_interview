6f3c920f17b5752711e8943f332d4596883d14c3
DRY-7717 <wicaksanabimaarya01@gmail.com>
2026-01-15 02:45:05 +0700

    feat: redirect to status list after update and customize success notification

--- app/Filament/Resources/StatusResource/Pages/EditStatus.php
+++ app/Filament/Resources/StatusResource/Pages/EditStatus.php
@@ -4,11 +4,25 @@ namespace App\Filament\Resources\StatusResource\Pages;
 
 use App\Filament\Resources\StatusResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 
 class EditStatus extends EditRecord
 {
     protected static string $resource = StatusResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getSavedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Status updated')
+            ->body('Status updated successfully.');
+    }
 
     protected function getHeaderActions(): array
     {
