106a15482b664ff4f9d101c640444d437f438232
DRY-7717 <wicaksanabimaarya01@gmail.com>
2026-01-15 05:15:24 +0700

    feat: redirect to severity list after update and customize success notification

--- app/Filament/Resources/SeverityResource/Pages/EditSeverity.php
+++ app/Filament/Resources/SeverityResource/Pages/EditSeverity.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\SeverityResource\Pages;
 
 use App\Filament\Resources\SeverityResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 
 class EditSeverity extends EditRecord
@@ -15,5 +16,18 @@ class EditSeverity extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getSavedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Severity updated')
+            ->body('Severity updated successfully.');
     }
 }
