2220cbe15bcd361bc43b290e6b9446666c0f49cf
DRY-7717 <wicaksanabimaarya01@gmail.com>
2026-01-15 01:15:50 +0700

    feat: Create 3 user accounts: 1 administrator and 2 developers in DatabaseSeeder.php and change the password encryption driver to argon2id.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +21,29 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+
+        User::create(
+            [
+                'email' => 'administrator@gmail.com',
+                'name' => "Administrator",
+                'password' => Hash::make('password'),
+                'role' => 'admin'
+            ],
+        );
+        User::create(
+            [
+                'email' => 'developerone@gmail.com',
+                'name' => "Developer One",
+                'password' => Hash::make('password')
+            ],
+        );
+        User::create(
+            [
+                'email' => 'developertwo@gmail.com',
+                'name' => "Developer Two",
+                'password' => Hash::make('password')
+            ],
+        );
     }
 }
