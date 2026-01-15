555b6d59d45d9978770ba938bc4943fe62116439
DRY-7717 <wicaksanabimaarya01@gmail.com>
2026-01-15 15:42:45 +0700

    feat: Add relation in model tasks, severity, status and user

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -4,10 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Status extends Model
 {
     use HasFactory;
 
     protected $guarded = ['id'];
+
+
+
+    public function task(): HasMany
+    {
+        return $this->hasMany(Tasks::class, 'status_id', 'id');
+    }
 }
--- app/Models/Tasks.php
+++ app/Models/Tasks.php
@@ -4,9 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Tasks extends Model
 {
     use HasFactory;
+    protected $table = 'tasks';
     protected $guarded = ['id'];
+
+
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'developer_id', 'id');
+    }
+    public function status(): BelongsTo
+    {
+        return $this->belongsTo(Status::class, 'status_id', 'id');
+    }
+    public function severity(): BelongsTo
+    {
+        return $this->belongsTo(Severity::class, 'severity_id', 'id');
+    }
 }
